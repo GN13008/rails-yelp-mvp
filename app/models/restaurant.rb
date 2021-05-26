95ec69b5aeabbff6d0bac71832c0568bba0077ba
Guillaume Negro <negro.guillaume@gmail.com>
2021-05-26 12:27:52 +0200

    correction of db

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,10 @@
 class Restaurant < ApplicationRecord
+  has_many :reviews
+  
+  # A restaurant must have a name, an address and a category.
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, presence: true
+
+  # A restaurant’s category must belong to this fixed list: ["chinese", "italian", "japanese", "french", "belgian"].
 end
--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,8 @@ ActiveRecord::Schema.define(version: 2021_05_26_101150) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
-    t.integer "phone_number"
+    t.string "phone_number"
+    t.string "category"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
